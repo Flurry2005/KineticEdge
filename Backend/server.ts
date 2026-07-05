5b83bee3b386953dbd4bc177b7a62f92cb64a150
Flurry <41346632+Flurry2005@users.noreply.github.com>
2026-07-05 16:45:40 +0200

    Fix

--- Backend/server.ts
+++ Backend/server.ts
@@ -10,3 +10,7 @@ const PORT: number = parseInt(process.env.PORT!) || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+setInterval(() => {
+  fetch("https://api.kineticedge.liamjorgensen.dev/").catch(() => {});
+}, 30_000);
