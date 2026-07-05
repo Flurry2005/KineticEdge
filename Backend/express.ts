515a3555c9ae52ff4beffd92fd59a27bd9a95e61
Flurry <41346632+Flurry2005@users.noreply.github.com>
2026-07-05 17:25:12 +0200

    Fix

--- Backend/express.ts
+++ Backend/express.ts
@@ -12,7 +12,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set("trust proxy", 1);
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:8000"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:8000",
+  "https://api.kineticedge.liamjorgensen.dev/",
+  "http://localhost:8000",
+  "https://kineticedge.liamjorgensen.dev/",
+];
 
 app.use(
   cors({
